113e59270a918f69afb88a6ee78d67a625ae9629
Peter <schmidt.multimedia@gmail.com>
2022-01-25 12:16:55 +0100

    Objekte 1 Daten/Funktionen / 1. Iteration

--- script_01.js
+++ script_01.js
@@ -1,30 +1,55 @@
 
 /***** Erinnerung: Arrays *******/
 
-let arr;
-arr = ["Ich","bin","Max"];
-output(arr);
-output(arr[0]);
-output("------------");
+// let arr;
+// arr = ["Ich","bin","Max"];
+// output(arr);
+// output(arr[0]);
+// output("------------");
 
-// nested arrays
-arr = [["Ich","bin","Max"],["Ich","bin","Moritz"]];
-output(arr[0][2]);
-output(arr[1][2]);
-output("------------");
+// // nested arrays
+// arr = [["Ich","bin","Max"],["Ich","bin","Moritz"]];
+// output(arr[0][2]);
+// output(arr[1][2]);
+// output("------------");
 
-// nested loops
-for (let i = 0; i < arr.length; i++) {
-       for (let j = 0; j < arr[0].length; j++) { 
-        output(arr[i][j]);          
-       }   
-}
+// // nested loops
+// for (let i = 0; i < arr.length; i++) {
+//        for (let j = 0; j < arr[0].length; j++) { 
+//         output(arr[i][j]);          
+//        }   
+// }
 
 
 /***** Objekte 1 Daten/Funktionen *******/
 
 
+let person = 
+        {
+                firstName:"Tilo",
+                familyName:"Schönemeyer",
+                salary:[120000,160000],
+                permission: true,
+                sayHello: 
+                        function() {
+                                return "Hello!";
+                        }
 
+
+        };
+
+
+output(person);
+person.firstName = "Thilo";
+output(person.firstName);
+output(person.familyName);
+
+const txt =     "Ich bin " + person.firstName + " " +
+                person.familyName + " und verdiene " +
+                person.salary[1] + " p.a."
+output(txt);
+
+output(person.sayHello());
 
 
 /** Ausgabe */
